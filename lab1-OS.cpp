80d02518b090c8bb43fa22ca8401855ddf416920
aleskitta <alesyadamasevich@gmail.com>
2025-09-16 11:54:11 +0300

    add thread suspension and number filtering
    - Added suspend/resume functionality with custom delay
    - Implemented filtering of numbers divisible by 5 in worker thread
    - Improved data structure to store both input and results

--- lab1-OS.cpp
+++ lab1-OS.cpp
@@ -1,22 +1,25 @@
 #include <Windows.h>
 #include <iostream>
+#include <process.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 struct ThreadData {
     vector<int> numbers;
+    vector<int> multiples;
 };
 
 unsigned __stdcall WorkerThread(void* param) {
     ThreadData* data = (ThreadData*)param;
+    vector<int>& arr = data->numbers;
 
-    cout << "Worker thread: processing numbers: ";
-    for (int num : data->numbers) {
-        cout << num << " ";
-        Sleep(50);
+    for (int num : arr) {
+        if (num % 5 == 0) {
+            data->multiples.push_back(num);
+        }
     }
-    cout << endl;
 
     return 0;
 }
@@ -32,16 +35,23 @@ int main() {
         cin >> numbers[i];
     }
 
+    int suspend_time;
+    cout << "Enter suspend time (ms): ";
+    cin >> suspend_time;
+
     ThreadData data;
     data.numbers = numbers;
 
-    HANDLE worker = CreateThread(
+    HANDLE worker;
+    unsigned thread_ID;
+
+    worker = (HANDLE)_beginthreadex(
         NULL,
         0,
-        (LPTHREAD_START_ROUTINE)WorkerThread,
+        &WorkerThread,
         &data,
         0,
-        NULL
+        &thread_ID
     );
 
     if (worker == NULL) {
@@ -49,9 +59,20 @@ int main() {
         return 1;
     }
 
+    SuspendThread(worker);
+    cout << "Thread suspended. Sleeping for " << suspend_time << " ms..." << endl;
+    Sleep(suspend_time);
+    ResumeThread(worker);
     WaitForSingleObject(worker, INFINITE);
-    CloseHandle(worker);
 
-    cout << "Thread finished" << endl;
+    cout << "Elements divisible by 5: ";
+    if (data.multiples.empty()) cout << "none found";
+    else {
+        for (int num : data.multiples)
+            cout << num << " ";
+    }
+    cout << endl;
+
+    CloseHandle(worker);
     return 0;
 }
